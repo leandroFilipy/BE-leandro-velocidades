5c86caf05013581d47960c183d09f1af51e11fcf
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-04-30 17:44:50 -0300

    feat: comeca

---
+++ src/main/java/com/atividade/leandro_velocidades/LeandroVelocidadesApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.leandro_velocidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeandroVelocidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeandroVelocidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividade/leandro_velocidades/LeandroVelocidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.leandro_velocidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeandroVelocidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
